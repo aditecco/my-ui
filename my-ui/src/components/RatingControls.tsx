4ee10b0f9675c50d5c8a3ab159d3ca97a7b7be6d
Alessandro Di Tecco <alessandro@ditecco.me>
2021-02-10 01:04:10 +0100

    (re)-added RatingControls.tsx, RatingControls.stories.tsx

---
+++ my-ui/src/components/RatingControls.tsx
@@ -0,0 +1,117 @@
+/* ---------------------------------
+RatingControls
+--------------------------------- */
+
+import * as React from "react";
+import MaterialIcon from "../_import/MaterialIcon/MaterialIcon";
+import styled from "@emotion/styled";
+import { useState } from "react";
+
+interface OwnProps {
+  initialRating?: number | undefined;
+  maxRating?: number | undefined;
+  onRate?: (arg0) => void;
+}
+
+const RatingControls = styled.span`
+  position: absolute;
+  top: 5px;
+  left: 5px;
+  z-index: 2;
+  padding: 6px;
+  border-radius: 4px;
+  background-color: rgba(0, 0, 0, 0.7);
+
+  .RatingControlsButton {
+    background-color: transparent;
+    border: none;
+    outline: none;
+
+    //@include zoomOnHover(1.25);
+  }
+
+  i.material-icons {
+    color: gold;
+    font-size: 22px;
+    font-weight: bold;
+  }
+`;
+
+export default function ({ initialRating, maxRating = 5, onRate }: OwnProps) {
+  const [starred, setStarred] = useState(initRating(initialRating) || {});
+
+  /**
+   * initRating
+   */
+
+  function initRating(rating) {
+    if (!rating) return undefined;
+
+    let r = {};
+
+    for (let i = 0; i <= maxRating; i++) {
+      if (i <= rating - 1) {
+        r[i] = true;
+      }
+
+      //
+      else {
+        r[i] = false;
+      }
+    }
+
+    return r;
+  }
+
+  /**
+   * handleRating
+   */
+  function handleRating(e: React.MouseEvent<HTMLButtonElement>) {
+    const { id } = e.currentTarget;
+    const currentStar = Number(id);
+
+    if (!starred[currentStar + 1]) {
+      let stars = {};
+
+      for (let i = 0; i <= currentStar; i++) {
+        if (!starred[currentStar]) {
+          stars[i] = true;
+        }
+
+        //
+        else {
+          stars[i] = false;
+        }
+      }
+
+      setStarred(stars);
+
+      onRate &&
+        onRate(
+          Object.values(stars).every((star) => !star) ? 0 : currentStar + 1
+        );
+    }
+  }
+
+  return (
+    <RatingControls>
+      <div>
+        {Array(maxRating)
+          .fill("")
+          .map((_, i) => (
+            <button
+              className="RatingControlsButton"
+              key={i}
+              id={String(i)}
+              onClick={handleRating}
+              data-starred={starred[String(i)]}
+            >
+              <MaterialIcon
+                icon={starred[String(i)] ? "star" : "star_outline"}
+              />
+            </button>
+          ))}
+      </div>
+    </RatingControls>
+  );
+}
---
+++ my-ui/src/stories/RatingControls.stories.tsx
@@ -0,0 +1,18 @@
+/* ---------------------------------
+RatingControls.stories.tsx
+--------------------------------- */
+
+import * as React from "react";
+import { Story, Meta } from "@storybook/react/types-6-0";
+import RatingControls from "../components/RatingControls";
+
+export default {
+  title: "Example/RatingControls",
+  component: RatingControls,
+  argTypes: {},
+} as Meta;
+
+const Template: Story<{}> = (args) => <RatingControls {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {};
